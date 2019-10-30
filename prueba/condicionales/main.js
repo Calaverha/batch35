4d8d79d688010866ac55a83cd7e1dd3a768c55af
Calaverha <rafferick@gmail.com>
2019-10-29 21:54:47 -0600

    modificaciones

---
+++ prueba/condicionales/main.js
@@ -0,0 +1,80 @@
+/*if(condicion){
+    comandos..
+}
+else if (condicion){
+COMANDOS..
+}
+let edad=18;
+if(edad===18){
+    console.log('eres mayor de edad');
+}else{
+    console.log('no eres mayor de edad'):
+}*/
+/*
+let numero=prompt( 'introduce un numero');
+if( numero%2===0)
+{
+console.log( "tu numero es par")
+}else {
+console.log("tu numero es impar")
+}
+
+dia=prompt('introduce un numero para los dias de la semana');
+let (dia ===0){
+    console.log("lunes");
+} else if{ (dia===1)console log('martes')}
+else if{ (dia===2)console log('miercoles')}
+else if{ (dia===3)console log('jueves')}
+else if{ (dia===4)console log('viernes')}
+else if{ (dia===5)console log('sabado')}
+else if{ (dia===6)console log('domingo')}
+else console log('martes')
+
+
+
+edad= prompt("que edad tienes")
+
+if 
+(edad <=15) 
+{
+    console.log('tu no puedes manejar')
+}
+else if 
+(edad  >=16  && edad <18)
+{
+    console.log('necesitas un Permiso')
+}
+else if
+ (edad >= 18 && edad <65)
+{
+    console.log('necesitas una licencia')
+}
+else if
+ (edad >= 65)
+{
+    console.log('ya no puedes manejar')
+}
+65
+
+
+player1= prompt('Piedra papel o tijeras P1'.toLowerCase())
+player2= prompt('Piedra papel o tijeras P2'.toLowerCase())
+
+if (player1 == 'piedra' && player2 == 'tijera') {console.log('gana PLAYER1')}
+else if (player1 == 'piedra' && player2 == 'papel') {console.log('gana PLAYER2')}
+else if (player1 == 'papel' && player2 == 'piedra') {console.log('gana player1')}
+else if (player1 == 'papel' && player2 == 'tijera') {console.log('gana player2')}
+else if (player1 == 'tijera' && player2 == 'papel') {console.log('gana player1')}
+else if  (player1 == 'tijera' && player2 == 'piedra') {console.log('gana player2')}
+else if (player2 == 'piedra' && player2 == 'tijera') {console.log('gana PLAYER2')}
+else if (player2 == 'papel' && player2 == 'piedra') {console.log('gana player2')}
+else if (player2 == 'tijera' && player2 == 'papel') {console.log('gana player2')}
+else if (player1 == player2) {console.log('empate')}
+else  {console.log('error')}
+*/
+
+password=prompt('introduce tu contraseña')
+if(password=='perritoSalchicha123'){console.log('Acceso aprobado')}
+else{console.error('acceso denegado')}
+
+campus.devf.la

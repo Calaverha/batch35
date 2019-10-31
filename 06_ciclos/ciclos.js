c6a0ba80500bcadccfc413e7a6a4d8315881e5b9
Calaverha <rafferick@gmail.com>
2019-10-30 21:51:35 -0600

    ciclos

---
+++ 06_ciclos/ciclos.js
@@ -0,0 +1,37 @@
+/*for(i=-763; i<=15; i=i+3){
+    console.log(i);
+}
+for(j=10; j0; j=j+5){
+    console.log(j);
+
+}for(k=500; k>=0; k=k-2){
+    console.log(k);
+}
+//ecma script es el best practices de java
+//suma 1= 1++
+
+
+
+for(var i=1; i<=100; i++)
+
+if (i%3===0 && i%5===0)
+{
+    console.log(i+'fizzbuzz')
+}
+else if(i%3===0)
+    {
+        console.log(i+'fizz')
+    }
+else if(i%5===0)
+{
+    console.log(i+'buzz');
+}
+else {
+    console.log(i)
+}
+
+*/
+
+for(i=1; i<=10; i++ )
+
+console.log(i)
---
+++ introducción a javascript/main.js
@@ -0,0 +1,106 @@
+//comprobar si esta bien linkeada
+//alert("hola");
+//var numero=10 
+//let numero = 10
+//valor strig o texto
+//let texto = "Rafael";
+//let texto2 = "rafa";
+//valor booleano https: eloquent javascript.net
+//let verdadero = true;
+//let falso = false;
+// objeto 
+//let object = {}
+//arreglo
+//let array ;
+//let x = 100; 
+//let y = 50;
+//-------------------------------------------------------------------
+//let suma = x+y;
+//console.log(suma)
+// + - * / %
+//let cambiar = 1500
+//console.log(cambiar);
+//cambiar=10;
+//console.log(cambiar);
+//cambiar="txto";
+//console.log(cambiar);
+//console.log(texto2)
+//Arrais
+//let frutas=["melon","sandia","fresa"]
+//frutas[3]="manzana";
+//frutas.push("guayaba");
+//frutas.pop()
+//frutas.pop()
+//frutas.splice(1,2)
+//console.log(frutas);
+//object
+/*
+let persona = {
+    nombre: "Rafael",
+    direccion : "obrera",
+    telefono : " 4206971000    ",
+    email: "rafa.4000@gmail.com",
+    edad: "30",
+    trabajo : ["dev.f","developer","e"],
+   musica:{
+        perreo:"la gata",
+        metal:"toxicity",
+        reggae:"jammin",
+    }
+ }
+console.log(persona.musica.metal);
+*/
+/*
+prompt(nombre);
+var nombre = prompt
+prompt(telefono);
+var telefono
+prompt(edad);
+var edad
+	{	
+  let nombre=prompt('Ingrese su nombre:','');
+  let edad=prompt('Ingrese su edad:','');
+  let telefono=prompt('Ingrese su telefono:','');
+    console.log("edad"+edad);
+    console.log("nombre"+nombre);
+    console.log("telefono"+telefono)
+	}
+{
+var edad1
+var edad2
+   edad1=prompt('en que año naciste:','')
+       edad2=2019-edad1
+    console.log(edad2)
+}*/
+let nombre=prompt('Ingrese su nombre:','');
+let edad =prompt("ingrese su edad");
+let telefono= prompt("ingrese su telefono");
+  let peliculas=prompt('Ingrese su pelicula favorita:','');
+  let musica=prompt('Ingrese su canción favorita:','');
+  let telefono=prompt('Ingrese su telefono:','');
+console.log(peliculas,musica,telefono)
+console.log(nombre,edad,telefono),,,,
+
+
+condicionales
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
